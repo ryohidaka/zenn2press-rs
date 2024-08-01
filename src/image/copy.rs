e98dccf4fd6b9489cbc7b2acc17dea229c0379b3
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-08-02 08:49:51 +0900

    test: Add test for copy_files()

--- src/image/copy.rs
+++ src/image/copy.rs
@@ -37,3 +37,52 @@ pub fn copy_files(file_paths: &[PathBuf], src_dir: &str, dest_dir: &str) -> io::
     println!("Copying of files is complete.");
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::{self, File};
+    use std::io::Write;
+
+    #[test]
+    fn test_copy_files() {
+        let temp_dir = tempfile::tempdir().unwrap();
+        let temp_dir_path = temp_dir.path();
+
+        let src_dir = temp_dir_path.join("src");
+        let dest_dir = temp_dir_path.join("dest");
+
+        fs::create_dir_all(&src_dir).unwrap();
+        fs::create_dir_all(&dest_dir).unwrap();
+
+        // Create test files
+        let file1 = src_dir.join("file1.txt");
+        let file2 = src_dir.join("file2.txt");
+        let mut f1 = File::create(&file1).unwrap();
+        writeln!(f1, "Test file 1").unwrap();
+        let mut f2 = File::create(&file2).unwrap();
+        writeln!(f2, "Test file 2").unwrap();
+
+        // Test copying files
+        let file_paths = vec![file1.clone(), file2.clone()];
+        copy_files(
+            &file_paths,
+            src_dir.to_str().unwrap(),
+            dest_dir.to_str().unwrap(),
+        )
+        .unwrap();
+
+        let copied_file1 = dest_dir.join("file1.txt");
+        let copied_file2 = dest_dir.join("file2.txt");
+
+        assert!(copied_file1.exists());
+        assert!(copied_file2.exists());
+
+        // Verify contents of the copied files
+        let contents1 = fs::read_to_string(copied_file1).unwrap();
+        let contents2 = fs::read_to_string(copied_file2).unwrap();
+
+        assert_eq!(contents1.trim(), "Test file 1");
+        assert_eq!(contents2.trim(), "Test file 2");
+    }
+}
