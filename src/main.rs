699865d7ba7f959797871f037255c56971539f3d
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-08-02 20:50:24 +0900

    feat: Print status.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
+use std::time::Instant;
+
+use colored::Colorize;
+use indicatif::HumanDuration;
 use utils::property::get_properties;
 use zenn2press::{image::copy_images, markdown::copy_markdown_files};
 
 mod constants;
-
 mod utils;
 
 #[tokio::main]
 async fn main() {
-    // Fetch properties from configuration
+    // Record the start time of the process
+    let started = Instant::now();
+
+    // Fetch properties from the configuration
     let properties = get_properties();
 
     // Convert Vec<String> to Vec<&str> for include and exclude lists
@@ -20,7 +26,16 @@ async fn main() {
         .as_ref()
         .map(|v| v.iter().map(|s| s.as_str()).collect());
 
+    // Print status update for copying markdown files
+    println!(
+        "{} {} {}",
+        "[2/3]".bright_black().bold(),
+        "📚",
+        "Copy markdown files:".bold().underline()
+    );
+
     // Copy markdown files from srcDir to destDir using properties
+    // If an error occurs, print the error message
     copy_markdown_files(
         properties.src_articles_dir.as_str(),
         properties.dest_articles_dir.as_str(),
@@ -32,7 +47,16 @@ async fn main() {
         eprintln!("Error copying markdown files: {}", e);
     });
 
+    // Print status update for copying image files
+    println!(
+        "{} {} {}",
+        "[3/3]".bright_black().bold(),
+        "🖼️",
+        "Copy image files:".bold().underline()
+    );
+
     // Copy images from srcDir to destDir using properties
+    // Await the result and handle success or error
     match copy_images(
         properties.src_images_dir.as_str(),
         properties.dest_images_dir.as_str(),
@@ -41,7 +65,7 @@ async fn main() {
     )
     .await
     {
-        Ok(_) => println!("Images copied successfully."),
+        Ok(_) => println!("\n\n{} Done in {}", "✨", HumanDuration(started.elapsed())),
         Err(e) => eprintln!("Error copying images: {}", e),
     }
 }
--- src/utils/property.rs
+++ src/utils/property.rs
@@ -1,5 +1,6 @@
 use args::Args;
 use clap::Parser;
+use colored::Colorize;
 
 use crate::constants::{ARTICLES_DIR, IMAGES_DIR};
 
@@ -28,31 +29,93 @@ pub fn get_properties() -> Properties {
     // Parse the command-line arguments into an `Args` struct
     let args = Args::parse();
 
-    // Derive paths for the source articles and images
-    let src_articles_dir = format!("{}{}", args.src_dir, ARTICLES_DIR);
-    let src_images_dir = format!("{}{}", args.src_dir, IMAGES_DIR);
-
-    // Clone the destination articles directory path
-    let dest_articles_dir = args.dest_dir.clone();
-
     // Print configuration details for debugging
-    println!("Configuration File: {:?}", args.config_file.as_deref());
-    println!(
-        "Articles: {:?} => {:?}",
-        src_articles_dir, dest_articles_dir
-    );
-    println!("Images: {:?} => {:?}", src_images_dir, args.dest_images_dir);
-    println!("Include Files: {:?}", args.include);
-    println!("Exclude Files: {:?}", args.exclude);
+    print_configuration_details(&args);
 
     // Return the populated `Properties` struct
     Properties {
         config_file: args.config_file,
-        src_articles_dir,
-        dest_articles_dir,
-        src_images_dir,
+        src_articles_dir: format!("{}{}", args.src_dir, ARTICLES_DIR),
+        dest_articles_dir: args.dest_dir.clone(),
+        src_images_dir: format!("{}{}", args.src_dir, IMAGES_DIR),
         dest_images_dir: args.dest_images_dir,
         include: Some(args.include),
         exclude: Some(args.exclude),
+    }
+}
+
+/// Prints the configuration details, including the configuration file,
+/// articles and images directories, and included/excluded files.
+fn print_configuration_details(args: &Args) {
+    println!(
+        "{} {} {}",
+        "[1/3]".bright_black().bold(),
+        "📝",
+        "Configuration Details:".bold().underline()
+    );
+
+    // Print config file
+    print_config_file(&args.config_file);
+
+    // Print articles and images directories
+    print_directories(&args.src_dir, &args.dest_dir);
+
+    // Print include and exclude files
+    print_include_exclude_files(&args.include, &args.exclude);
+}
+
+/// Prints the configuration file path or "None" if not available.
+fn print_config_file(config_file: &Option<String>) {
+    match config_file {
+        Some(config) => println!("- {} {}", "Configuration File:".green().bold(), config),
+        None => println!(
+            "- {} {}",
+            "Configuration File:".green().bold(),
+            "None".yellow().bold()
+        ),
+    }
+}
+
+/// Prints the source and destination directories for articles and images.
+fn print_directories(src_dir: &str, dest_dir: &str) {
+    let src_articles_dir = format!("{}{}", src_dir, ARTICLES_DIR);
+    let src_images_dir = format!("{}{}", src_dir, IMAGES_DIR);
+
+    println!(
+        "- {} {:?} {} {:?}",
+        "Articles:".green().bold(),
+        src_articles_dir,
+        "=>".yellow().bold(),
+        dest_dir
+    );
+    println!(
+        "- {} {:?} {} {:?}",
+        "Images:".green().bold(),
+        src_images_dir,
+        "=>".yellow().bold(),
+        dest_dir
+    );
+}
+
+/// Prints the list of included and excluded files, or "None" if the lists are empty.
+fn print_include_exclude_files(include: &Vec<String>, exclude: &Vec<String>) {
+    // Include Files handling
+    println!("- {}", "Include Files:".green().bold());
+    if include.is_empty() {
+        println!("\t- {}", "None");
+    } else {
+        for file in include {
+            println!("\t- {}", file);
+        }
+    }
+
+    // Exclude Files handling
+    println!("- {}", "Exclude Files:".green().bold());
+    if exclude.is_empty() {
+        println!("\t- {}", "None");
+    } else {
+        for file in exclude {
+            println!("\t- {}", file);
+        }
     }
 }
